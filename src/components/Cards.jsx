d14bc2d0e1fb5ed71c9727efa7ec1f1fcb07bdaa
Andrés Rodríguez <artdavid2022@gmail.com>
2023-05-23 00:06:52 -0500

    cambio de posición cards img/video

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,11 +17,11 @@ const Cards = () => {
             <Card.Text className="mb-0" style={{textAlign: 'right', fontSize: '10px', color: '#1DA5B4'}}>{produced} PELÍCULAS PRODUCIDAS</Card.Text>
         </Card.Body>
         </Card>
-        <Card style={{ width: '30vw', height:'23.5vh', backgroundColor: '#111111', margin: '2rem 0rem 1rem 0rem', border: '#42331A 1px solid', flexGrow: 1}}>
-            <Card.Img style={{width: '100%', height: '100%'}} src='https://hips.hearstapps.com/hmg-prod/images/d1pklzbuyaab0la-1552597012.jpg?crop=1xw:0.5064697265625xh;center,top&resize=1200:*'></Card.Img>
-        </Card>
-        <Card style={{ width: '30vw', height:'100%', backgroundColor: '#111111', margin: '2rem 3.5rem 1rem 1rem', border: '#42331A 1px solid', flexGrow: 1}}>
+        <Card style={{ width: '30vw', height:'100%', backgroundColor: '#111111', margin: '2rem 0rem 1rem 0rem', border: '#42331A 1px solid', flexGrow: 1}}>
         <iframe height='100%' width='100%' src="https://www.youtube.com/embed/u80gfKQpAUA?autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        </Card>
+        <Card style={{ width: '30vw', height:'23.5vh', backgroundColor: '#111111', margin: '2rem 3.5rem 1rem 1rem', border: '#42331A 1px solid', flexGrow: 1}}>
+            <Card.Img style={{width: '100%', height: '100%'}} src='https://hips.hearstapps.com/hmg-prod/images/d1pklzbuyaab0la-1552597012.jpg?crop=1xw:0.5064697265625xh;center,top&resize=1200:*'></Card.Img>
         </Card>
         
 
